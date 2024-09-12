e3a8db1a7886b8c1110aa756d5af48830b64644b
milagrosVillalba <soledad110203@gmail.com>
2024-09-12 16:32:36 -0300

    se estbana creando los modelos

--- backend/src/db/connect.ts
+++ backend/src/db/connect.ts
@@ -1,19 +1,21 @@
-import { Pool } from "pg";
-import { URI } from "../config/config";
+import { Sequelize } from 'sequelize';
+import { URI } from '../config/config';
 
-//crear un nuevo pool para manejar las conexiones a la base de datos 
-const pool = new Pool({
-    connectionString: URI, // Usar la variable de entorno URI
-  });
-  
-  // Función para conectarse a la base de datos
-  export const connectDB = async () => {
-    try {
-      const client = await pool.connect();
-      console.log('Conexión exitosa a la base de datos');
-      client.release(); // Liberar el cliente después de conectarse
-    } catch (error) {
-      console.error('Error al conectar a la base de datos:', error);
-      process.exit(1); // Salir si no se puede conectar
-    }
-  };
+// Configura Sequelize
+const sequelize = new Sequelize(URI as string, {
+  dialect: 'postgres',
+  logging: false, // Cambia a `console.log` para ver las consultas SQL
+});
+
+// Función para conectar a la base de datos
+export const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Conexión exitosa a la base de datos');
+  } catch (error) {
+    console.error('Error al conectar a la base de datos:', error);
+    process.exit(1); // Salir si no se puede conectar
+  }
+};
+
+export default sequelize;
