e0a2cb6375532e8adedd1fd4fd49f2515354775f
milagrosVillalba <soledad110203@gmail.com>
2025-04-23 11:21:55 -0300

    config

--- frond/inventariFrond/src/App.jsx
+++ frond/inventariFrond/src/App.jsx
@@ -1,14 +1,14 @@
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter  } from 'react-router-dom';
 import AppRouter from './routes/AppRoutes';
 import './index.css'; // Asegúrate de importar los estilos de Tailwind CSS aquí
 
 const App = () => {
   return (
-    <Router>
+    <BrowserRouter basename="/">
       <AppRouter />
-    </Router>
+   </BrowserRouter>
   );
 };
 
